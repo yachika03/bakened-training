b4d9424e58b8a10273bdd7214be2a6a82ff11e6d
Yachika mehta <yachikamehta03@gmail.com>
2022-08-06 14:15:04 +0530

    add logger

---
+++ src/logger/logger.js
@@ -0,0 +1,9 @@
+
+const info="Welcome to my application I am Yachika Mehta and a part of FunctionUp Plutonium cohort."
+
+ function welcome ()  {
+     console.log(info)
+
+ }
+
+module.exports.myInfo=welcome
