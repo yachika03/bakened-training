c116454ce2254ed02819828ecc5978853816de7a
Yachika mehta <yachikamehta03@gmail.com>
2022-08-13 21:43:00 +0530

    add this

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
+const{default:mongoose} =require("mongoose");
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/', route);
 
+mongoose.connect("mongodb+srv://yachika03:wkaTIq3zkjIou3YI@cluster0.t9qdtvx.mongodb.net/yachika03-DB?retryWrites=true&w=majority",{
+useNewUrlParser:true
+})
+.then(( )=>console.log("MongoDB is connected"))
+.catch(err=>console.log(err))
+
+
+app.use('/', route);
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
