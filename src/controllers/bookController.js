33456769a6ce9e3fc52aef2e28a9f0d3c5a662e9
Saquib <saquibparvez007@gmail.com>
2022-08-25 10:19:11 +0530

    middleware

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -2,40 +2,55 @@ const bookModel= require("../models/bookModel")
 const AuthorModel= require("../models/authorModel")
 const PublisherModel= require("../models/publisherModel")
 const mongoose = require('mongoose');
-const { find } = require("../models/bookModel");
 const publisherModel = require("../models/publisherModel");
+const authorModel = require("../models/authorModel");
 
 const ObjectId=mongoose.Schema.Types.ObjectId
 
 
 const createBook= async function (req, res) {
     const book = req.body
-    const author_id=AuthorModel.name
-    const publisher_id=bookModel.publisher_id
-    let bookCreated = await bookModel.create(book)
+    const authorId=book.author_id
+    const publisherId=book.publisher_id
    
+    if (!authorId){
+        return res.send({status:false,msg:"Author_id is mandatory"})
     
-   const validObjectId=function(ObjectId){
-    return mongoose.Types.ObjectId.isValid("6300ac64e921b6e85be57ac7")
-   }
-   if(!validObjectId(author_id)){
-    return res.send({ErrorMessage:"Id Not Valid"})
-   }else{
+    }
+    const author=await AuthorModel.findById(authorId)
+    if(!author){
+        return res.send({status:false,msg:"Author not found"})
+
+    }
+    if (!publisherId){
+        return res.send({status:false,msg:"Publsiher_id is mandatory"})
+    
+    }
+    const publisher=await PublisherModel.findById(publisherId)
+    if(!publisher){
+        return res.send({status:false,msg:"publisher not found"})
+
+    }
+    let bookCreated = await bookModel.create(book)
     res.send({data: bookCreated})
 
-   }
-        
+   
 }
 
-const update = { isHardCover:true };
-const bookprice={ratings:{$gt:3.5}}
-const priceinc={$inc:{price:10}}
-const UpdateBook=async function(req,res){
-    let BOOK=await bookModel.findById('6300ab7ae921b6e85be57abd').updateMany(update)
-    let newbook=await bookModel.find(bookprice).updateMany(priceinc)
-    
 
-    res.send({data:newbook,BOOK})
+const UpdateBook=async function(req,res){
+
+    let requiredPublishers = 
+    await publisherModel.find({$or: [{name: "Penguin"},{name: "HarperCollins"}]}, {_id: 1})
+    //let books = await bookModel.find().populate('publisher')
+    //for
+    let requiredPublisherIds = [] 
+    for (let i = 0; i < requiredPublishers.length; i++) {
+        requiredPublisherIds.push(requiredPublishers[i]._id)
+    }
+
+    let BOOK=await bookModel.updateMany({publisher : {$in: requiredPublisherIds}}, {isHardCover: true}, {new: true})
+    res.send({data: requiredPublishers})
 }
 
 
